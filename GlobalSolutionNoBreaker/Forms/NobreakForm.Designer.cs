c72a96fec2d717a8a3ad6e9d4a1a5356fafcd393
G3n4r0 <rm551986@fiap.com.br>
2025-06-05 13:41:53 -0300

    Ajuste pagina bnobreaks

--- GlobalSolutionNoBreaker/Forms/NobreakForm.Designer.cs
+++ GlobalSolutionNoBreaker/Forms/NobreakForm.Designer.cs
@@ -29,16 +29,16 @@
         private void InitializeComponent()
         {
             dgvNobreak = new DataGridView();
-            btnAdicionar = new Button();
-            btnExcluir = new Button();
-            btnLimpar = new Button();
-            btnVoltar = new Button();
-            lblModelo = new Label();
-            lblLocal = new Label();
-            lblAquisicao = new Label();
             dtpAquisicao = new DateTimePicker();
-            cmbModelo = new ComboBox();
             cmbLocal = new ReaLTaiizor.Controls.HopeComboBox();
+            cmbModelo = new ReaLTaiizor.Controls.HopeComboBox();
+            btnVoltarNobreak = new ReaLTaiizor.Controls.HopeRoundButton();
+            btnLimpar = new ReaLTaiizor.Controls.HopeRoundButton();
+            btnExcluir = new ReaLTaiizor.Controls.HopeRoundButton();
+            btnAdicionar = new ReaLTaiizor.Controls.HopeRoundButton();
+            headerLabel1 = new ReaLTaiizor.Controls.HeaderLabel();
+            headerLabel2 = new ReaLTaiizor.Controls.HeaderLabel();
+            headerLabel3 = new ReaLTaiizor.Controls.HeaderLabel();
             ((System.ComponentModel.ISupportInitialize)dgvNobreak).BeginInit();
             SuspendLayout();
             // 
@@ -54,93 +54,13 @@
             dgvNobreak.CellContentClick += dataGridView1_CellContentClick;
             dgvNobreak.CellDoubleClick += dgvNobreak_CellDoubleClick;
             // 
-            // btnAdicionar
-            // 
-            btnAdicionar.Location = new Point(9, 544);
-            btnAdicionar.Margin = new Padding(3, 4, 3, 4);
-            btnAdicionar.Name = "btnAdicionar";
-            btnAdicionar.Size = new Size(86, 31);
-            btnAdicionar.TabIndex = 1;
-            btnAdicionar.Text = "Adicionar";
-            btnAdicionar.UseVisualStyleBackColor = true;
-            btnAdicionar.Click += btnAdicionar_Click;
-            // 
-            // btnExcluir
-            // 
-            btnExcluir.Location = new Point(155, 544);
-            btnExcluir.Margin = new Padding(3, 4, 3, 4);
-            btnExcluir.Name = "btnExcluir";
-            btnExcluir.Size = new Size(86, 31);
-            btnExcluir.TabIndex = 2;
-            btnExcluir.Text = "Excluir";
-            btnExcluir.UseVisualStyleBackColor = true;
-            btnExcluir.Click += btnExcluir_Click;
-            // 
-            // btnLimpar
-            // 
-            btnLimpar.Location = new Point(320, 544);
-            btnLimpar.Margin = new Padding(3, 4, 3, 4);
-            btnLimpar.Name = "btnLimpar";
-            btnLimpar.Size = new Size(86, 31);
-            btnLimpar.TabIndex = 3;
-            btnLimpar.Text = "Limpar";
-            btnLimpar.UseVisualStyleBackColor = true;
-            btnLimpar.Click += btnLimpar_Click;
-            // 
-            // btnVoltar
-            // 
-            btnVoltar.Location = new Point(466, 544);
-            btnVoltar.Margin = new Padding(3, 4, 3, 4);
-            btnVoltar.Name = "btnVoltar";
-            btnVoltar.Size = new Size(86, 31);
-            btnVoltar.TabIndex = 4;
-            btnVoltar.Text = "Voltar";
-            btnVoltar.UseVisualStyleBackColor = true;
-            btnVoltar.Click += btnVoltar_Click;
-            // 
-            // lblModelo
-            // 
-            lblModelo.AutoSize = true;
-            lblModelo.Location = new Point(40, 400);
-            lblModelo.Name = "lblModelo";
-            lblModelo.Size = new Size(61, 20);
-            lblModelo.TabIndex = 5;
-            lblModelo.Text = "Modelo";
-            // 
-            // lblLocal
-            // 
-            lblLocal.AutoSize = true;
-            lblLocal.Location = new Point(208, 464);
-            lblLocal.Name = "lblLocal";
-            lblLocal.Size = new Size(125, 20);
-            lblLocal.TabIndex = 6;
-            lblLocal.Text = "Localização física";
-            // 
-            // lblAquisicao
-            // 
-            lblAquisicao.AutoSize = true;
-            lblAquisicao.Location = new Point(440, 400);
-            lblAquisicao.Name = "lblAquisicao";
-            lblAquisicao.Size = new Size(131, 20);
-            lblAquisicao.TabIndex = 8;
-            lblAquisicao.Text = "Data de Aquisição";
-            // 
             // dtpAquisicao
             // 
-            dtpAquisicao.Location = new Point(584, 400);
+            dtpAquisicao.Location = new Point(600, 416);
             dtpAquisicao.Margin = new Padding(3, 4, 3, 4);
             dtpAquisicao.Name = "dtpAquisicao";
             dtpAquisicao.Size = new Size(292, 27);
             dtpAquisicao.TabIndex = 16;
-            // 
-            // cmbModelo
-            // 
-            cmbModelo.FormattingEnabled = true;
-            cmbModelo.Location = new Point(144, 400);
-            cmbModelo.Margin = new Padding(3, 4, 3, 4);
-            cmbModelo.Name = "cmbModelo";
-            cmbModelo.Size = new Size(255, 28);
-            cmbModelo.TabIndex = 19;
             // 
             // cmbLocal
             // 
@@ -148,11 +68,135 @@
             cmbLocal.FlatStyle = FlatStyle.Flat;
             cmbLocal.Font = new Font("Segoe UI", 10F);
             cmbLocal.FormattingEnabled = true;
-            cmbLocal.ItemHeight = 30;
-            cmbLocal.Location = new Point(344, 464);
+            cmbLocal.ItemHeight = 24;
+            cmbLocal.Location = new Point(296, 488);
             cmbLocal.Name = "cmbLocal";
-            cmbLocal.Size = new Size(280, 36);
+            cmbLocal.Size = new Size(328, 30);
             cmbLocal.TabIndex = 21;
+            // 
+            // cmbModelo
+            // 
+            cmbModelo.DrawMode = DrawMode.OwnerDrawFixed;
+            cmbModelo.FlatStyle = FlatStyle.Flat;
+            cmbModelo.Font = new Font("Segoe UI", 10F);
+            cmbModelo.FormattingEnabled = true;
+            cmbModelo.ItemHeight = 24;
+            cmbModelo.Location = new Point(120, 416);
+            cmbModelo.Name = "cmbModelo";
+            cmbModelo.Size = new Size(280, 30);
+            cmbModelo.TabIndex = 22;
+            // 
+            // btnVoltarNobreak
+            // 
+            btnVoltarNobreak.BorderColor = Color.FromArgb(220, 223, 230);
+            btnVoltarNobreak.ButtonType = ReaLTaiizor.Util.HopeButtonType.Primary;
+            btnVoltarNobreak.DangerColor = Color.FromArgb(245, 108, 108);
+            btnVoltarNobreak.DefaultColor = Color.FromArgb(255, 255, 255);
+            btnVoltarNobreak.Font = new Font("Segoe UI", 12F);
+            btnVoltarNobreak.HoverTextColor = Color.FromArgb(48, 49, 51);
+            btnVoltarNobreak.InfoColor = Color.FromArgb(144, 147, 153);
+            btnVoltarNobreak.Location = new Point(16, 552);
+            btnVoltarNobreak.Name = "btnVoltarNobreak";
+            btnVoltarNobreak.PrimaryColor = Color.FromArgb(64, 158, 255);
+            btnVoltarNobreak.Size = new Size(128, 32);
+            btnVoltarNobreak.SuccessColor = Color.FromArgb(103, 194, 58);
+            btnVoltarNobreak.TabIndex = 23;
+            btnVoltarNobreak.Text = "Voltar";
+            btnVoltarNobreak.TextColor = Color.White;
+            btnVoltarNobreak.WarningColor = Color.FromArgb(230, 162, 60);
+            // 
+            // btnLimpar
+            // 
+            btnLimpar.BorderColor = Color.FromArgb(220, 223, 230);
+            btnLimpar.ButtonType = ReaLTaiizor.Util.HopeButtonType.Primary;
+            btnLimpar.DangerColor = Color.FromArgb(245, 108, 108);
+            btnLimpar.DefaultColor = Color.FromArgb(255, 255, 255);
+            btnLimpar.Font = new Font("Segoe UI", 12F);
+            btnLimpar.HoverTextColor = Color.FromArgb(48, 49, 51);
+            btnLimpar.InfoColor = Color.FromArgb(144, 147, 153);
+            btnLimpar.Location = new Point(600, 552);
+            btnLimpar.Name = "btnLimpar";
+            btnLimpar.PrimaryColor = Color.FromArgb(64, 158, 255);
+            btnLimpar.Size = new Size(128, 32);
+            btnLimpar.SuccessColor = Color.FromArgb(103, 194, 58);
+            btnLimpar.TabIndex = 24;
+            btnLimpar.Text = "Limpar";
+            btnLimpar.TextColor = Color.White;
+            btnLimpar.WarningColor = Color.FromArgb(230, 162, 60);
+            // 
+            // btnExcluir
+            // 
+            btnExcluir.BorderColor = Color.FromArgb(220, 223, 230);
+            btnExcluir.ButtonType = ReaLTaiizor.Util.HopeButtonType.Primary;
+            btnExcluir.DangerColor = Color.FromArgb(245, 108, 108);
+            btnExcluir.DefaultColor = Color.FromArgb(255, 255, 255);
+            btnExcluir.Font = new Font("Segoe UI", 12F);
+            btnExcluir.HoverTextColor = Color.FromArgb(48, 49, 51);
+            btnExcluir.InfoColor = Color.FromArgb(144, 147, 153);
+            btnExcluir.Location = new Point(448, 552);
+            btnExcluir.Name = "btnExcluir";
+            btnExcluir.PrimaryColor = Color.FromArgb(64, 158, 255);
+            btnExcluir.Size = new Size(128, 32);
+            btnExcluir.SuccessColor = Color.FromArgb(103, 194, 58);
+            btnExcluir.TabIndex = 25;
+            btnExcluir.Text = "Excluir";
+            btnExcluir.TextColor = Color.White;
+            btnExcluir.WarningColor = Color.FromArgb(230, 162, 60);
+            // 
+            // btnAdicionar
+            // 
+            btnAdicionar.BorderColor = Color.FromArgb(220, 223, 230);
+            btnAdicionar.ButtonType = ReaLTaiizor.Util.HopeButtonType.Primary;
+            btnAdicionar.DangerColor = Color.FromArgb(245, 108, 108);
+            btnAdicionar.DefaultColor = Color.FromArgb(255, 255, 255);
+            btnAdicionar.Font = new Font("Segoe UI", 12F);
+            btnAdicionar.HoverTextColor = Color.FromArgb(48, 49, 51);
+            btnAdicionar.InfoColor = Color.FromArgb(144, 147, 153);
+            btnAdicionar.Location = new Point(760, 552);
+            btnAdicionar.Name = "btnAdicionar";
+            btnAdicionar.PrimaryColor = Color.FromArgb(64, 158, 255);
+            btnAdicionar.Size = new Size(128, 32);
+            btnAdicionar.SuccessColor = Color.FromArgb(103, 194, 58);
+            btnAdicionar.TabIndex = 26;
+            btnAdicionar.Text = "Adicionar";
+            btnAdicionar.TextColor = Color.White;
+            btnAdicionar.WarningColor = Color.FromArgb(230, 162, 60);
+            // 
+            // headerLabel1
+            // 
+            headerLabel1.AutoSize = true;
+            headerLabel1.BackColor = Color.Transparent;
+            headerLabel1.Font = new Font("Microsoft Sans Serif", 11F, FontStyle.Bold);
+            headerLabel1.ForeColor = Color.FromArgb(255, 255, 255);
+            headerLabel1.Location = new Point(128, 496);
+            headerLabel1.Name = "headerLabel1";
+            headerLabel1.Size = new Size(154, 18);
+            headerLabel1.TabIndex = 27;
+            headerLabel1.Text = "Localização Física:";
+            // 
+            // headerLabel2
+            // 
+            headerLabel2.AutoSize = true;
+            headerLabel2.BackColor = Color.Transparent;
+            headerLabel2.Font = new Font("Microsoft Sans Serif", 11F, FontStyle.Bold);
+            headerLabel2.ForeColor = Color.FromArgb(255, 255, 255);
+            headerLabel2.Location = new Point(32, 424);
+            headerLabel2.Name = "headerLabel2";
+            headerLabel2.Size = new Size(69, 18);
+            headerLabel2.TabIndex = 28;
+            headerLabel2.Text = "Modelo:";
+            // 
+            // headerLabel3
+            // 
+            headerLabel3.AutoSize = true;
+            headerLabel3.BackColor = Color.Transparent;
+            headerLabel3.Font = new Font("Microsoft Sans Serif", 11F, FontStyle.Bold);
+            headerLabel3.ForeColor = Color.FromArgb(255, 255, 255);
+            headerLabel3.Location = new Point(464, 424);
+            headerLabel3.Name = "headerLabel3";
+            headerLabel3.Size = new Size(126, 18);
+            headerLabel3.TabIndex = 29;
+            headerLabel3.Text = "Data Aquisição:";
             // 
             // NobreakForm
             // 
@@ -160,18 +204,16 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
             ClientSize = new Size(914, 600);
-            //
-            //
-            Controls.Add(cmbLocal);
-            Controls.Add(cmbModelo);
-            Controls.Add(dtpAquisicao);
-            Controls.Add(lblAquisicao);
-            Controls.Add(lblLocal);
-            Controls.Add(lblModelo);
-            Controls.Add(btnVoltar);
-            Controls.Add(btnLimpar);
-            Controls.Add(btnExcluir);
+            Controls.Add(headerLabel3);
+            Controls.Add(headerLabel2);
+            Controls.Add(headerLabel1);
             Controls.Add(btnAdicionar);
+            Controls.Add(btnExcluir);
+            Controls.Add(btnLimpar);
+            Controls.Add(btnVoltarNobreak);
+            Controls.Add(cmbModelo);
+            Controls.Add(cmbLocal);
+            Controls.Add(dtpAquisicao);
             Controls.Add(dgvNobreak);
             Margin = new Padding(3, 5, 3, 5);
             MinimumSize = new Size(518, 503);
@@ -186,16 +228,16 @@
         #endregion
 
         private DataGridView dgvNobreak;
-        private Button btnAdicionar;
-        private Button btnExcluir;
-        private Button btnLimpar;
-        private Button btnVoltar;
-        private Label lblModelo;
-        private Label lblLocal;
-        private Label lblAquisicao;
         private DateTimePicker dtpAquisicao;
-        private ComboBox cmbModelo;
         //private ComboBox cmbLocal;
         private ReaLTaiizor.Controls.HopeComboBox cmbLocal;
+        private ReaLTaiizor.Controls.HopeComboBox cmbModelo;
+        private ReaLTaiizor.Controls.HopeRoundButton btnVoltarNobreak;
+        private ReaLTaiizor.Controls.HopeRoundButton btnLimpar;
+        private ReaLTaiizor.Controls.HopeRoundButton btnExcluir;
+        private ReaLTaiizor.Controls.HopeRoundButton btnAdicionar;
+        private ReaLTaiizor.Controls.HeaderLabel headerLabel1;
+        private ReaLTaiizor.Controls.HeaderLabel headerLabel2;
+        private ReaLTaiizor.Controls.HeaderLabel headerLabel3;
     }
 }
